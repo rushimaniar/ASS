0bd7b0d3ad944142bd2b681c62f5445da057169b
SwapnilKumbhar <swapsk05@gmail.com>
2018-02-09 15:14:16 +0530

    Added Ugly

--- gatherer.py
+++ gatherer.py
@@ -10,12 +10,14 @@
 
 import cv2
 import urllib.request
+import os
+import numpy as np
 
 def getImages(url):
     links = urllib.request.urlopen(url).read().decode()
     links = links.split('\n')
 
-    idx = 1
+    idx = 645
     for link in links:
         try:
             print('[*] Fetching: '+str(link))
@@ -29,12 +31,42 @@ def getImages(url):
         except Exception as e:
             print('[!] Error at index: '+str(idx)+'\n'+str(e))
 
+def killUglies():
+    '''
+        Uglies are the images that are of no use to the training or in anyway useful. 
+        The most common example of uglies is a 404 image from flickr. It is one of the uglies we have chosen to be deleted.
+    '''
+    uglies = os.listdir('uglies')
+    imgs = os.listdir('neg')
+    for img in imgs:
+        for ugly in uglies:
+            try:
+                cimgp = 'neg/'+str(img)
+                ugimg = cv2.imread('uglies/'+str(ugly))
+                check = cv2.imread(cimgp)
+                if ugimg.shape == check.shape and not (np.bitwise_xor(ugimg, check).any()):
+                    # Ugly image detected. Remove it.
+                    print('[!] Ugly image detected: '+str(cimgp))
+                    print('[!] Deleting image...')
+                    os.remove(cimgp)
+            except Exception as e:
+                print('[-] Error: ' + str(e))
+
 
 if __name__ == '__main__':
     # URL of image dataset. Obtained from image-net.org. 
-    url = 'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02715513'
+    # url = 'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02715513'
+    
+    # URL of outdoor images
+    url = 'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n08613733'
+
     
     # Start getting images
     print('[+] Gathering Images...')
     getImages(url)
     print('[+] Done Gathering Images.')
+    
+    # Identify and remove all ugly images
+    print('[+] Removing ugly images...')
+    killUglies()
+    print('[+] Done.')
