a22d97d09a2a127d5f7a1d720c2a0059187442ab
SwapnilKumbhar <swapsk05@gmail.com>
2018-04-01 22:52:08 +0530

    Added mandatory dislaimer

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This project marks the beginning of an apocalypse brought about by the machines.
 All hail the apocalypse.
 
 This ends tonight.
+
+# Disclaimer
+Yeh project me bahut zyada jhol hai.
--- main.py
+++ main.py
@@ -9,11 +9,12 @@ from logger import *
 from random import *
 import string
 
-vid = base.VideoReader('data/crowd.mp4',2)
+vid = base.VideoReader('data/crowd.mp4',0.2)
 
 yolo_c = base.Y_Classifier('cfg/yolov3.cfg','cfg/coco.data','yolov3.weights',0.5)
 yolo_c.loadClassifier()
 
-for i in range(0, 5):
+for i in range(0, 15):
     frame = vid.next()
-    print yolo_c.detect('tmp/'+frame)
+    r =  yolo_c.detect('tmp/'+frame)
+    print r
